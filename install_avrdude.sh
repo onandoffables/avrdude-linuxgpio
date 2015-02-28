379ffc4f6999992ce90485f275b83244862fd796
onandoffables <on@onandoffables.com>
2015-02-28 23:50:58 +0100

    add install

---
+++ install_avrdude.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+deb=avrdude_6.1-2_armhf.deb
+
+if [ ! -f ${deb} ]; then
+	./make_deb.sh
+fi
+
+if [ -f ${deb} ]; then
+	sudo dpkg -i ${deb}
+fi
+
+# so we don't need sudo
+sudo chmod 4755 /usr/bin/avrdude
