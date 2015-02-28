b1dde4a16dd0c7b765909394b2a17abd2385f7f2
onandoffables <on@onandoffables.com>
2015-03-01 00:17:57 +0100

    var names

--- compile_avrdude.sh
+++ compile_avrdude.sh
@@ -2,35 +2,35 @@
 
 #sudo apt-get install libusb-1.0-0-dev libusb-dev libftdi-dev libftdi1 libelf-dev autoconf bison flex
 
-AVRDUDE_NAME=avrdude-6.1
+target=avrdude-6.1
 
-AVRDUDE_TGZ=${AVRDUDE_NAME}.tar.gz
-AVRDUDE_PATCH=patches/${AVRDUDE_NAME}.patch
-AVRDUDE_URL=http://download.savannah.gnu.org/releases/avrdude/${AVRDUDE_TGZ}
+tgz=${target}.tar.gz
+patch=patches/${target}.patch
+url=http://download.savannah.gnu.org/releases/avrdude/${tgz}
 
 # delete old directory
-if [ -d ${AVRDUDE_NAME} ]; then
-	echo "Found old ${AVRDUDE_NAME}: deleting..."
-	rm -rf ${AVRDUDE_NAME}
+if [ -d ${target} ]; then
+	echo "Found old ${target}: deleting..."
+	rm -rf ${target}
 fi
 
 # get tar.gz if necessary
-if [ ! -f ${AVRDUDE_TGZ} ]; then
-	echo "${AVRDUDE_TGZ} not found. Getting it..."
-	wget ${AVRDUDE_URL} || { echo "Error getting "${AVRDUDE_TGZ}; exit 1; }
+if [ ! -f ${tgz} ]; then
+	echo "${tgz} not found. Getting it..."
+	wget ${url} || { echo "Error getting "${tgz}; exit 1; }
 fi
 
 # extract
-if [ -f ${AVRDUDE_TGZ} ]; then
-	echo "Found ${AVRDUDE_TGZ}: extracting..."
-	tar -xzvf ${AVRDUDE_TGZ} || { echo "Error extracting "${AVRDUDE_TGZ}; exit 1; }
+if [ -f ${tgz} ]; then
+	echo "Found ${tgz}: extracting..."
+	tar -xzvf ${tgz} || { echo "Error extracting "${tgz}; exit 1; }
 fi
 
 # patch avrdude.conf.in
-patch -p1 -d ${AVRDUDE_NAME} < ${AVRDUDE_PATCH} || { echo "Error patching "${AVRDUDE_NAME}; exit 1; }
+patch -p1 -d ${target} < ${patch} || { echo "Error patching "${target}; exit 1; }
 
 # bootstrap, compile, install
-cd ${AVRDUDE_NAME}
+cd ${target}
 ./bootstrap
 ./configure --enable-linuxgpio=yes --prefix=/usr --sysconfdir=/etc
 make
